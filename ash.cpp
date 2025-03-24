0ba07e5199595c84a4a750c21d6e18f621bdbb57
Aliqyan-21 <aliqyanabid21@gmail.com>
2025-03-25 00:38:29 +0530

    feat: history support with up and down arrows and left and right arrow support for editing

--- ash.cpp
+++ ash.cpp
@@ -1,17 +1,95 @@
 #include "ash.h"
-#include "ash_execvp.h"
-#include <csignal>
 
 /* constructor
 constructor to initialize the ash class */
-Ash::Ash() {}
+Ash::Ash() : history_idx(-1) {}
+
+/* get_char
+this function is to get character input without pressing enter */
+char Ash::get_char() {
+  struct termios oldt, newt;
+  char ch;
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  ch = getchar();
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  return ch;
+}
 
 /* read line function
-this function read lines from the buffer */
+this function read lines from the buffer, supports arrow keys and history */
 std::string Ash::read_line() {
   std::string line;
-  getline(std::cin, line);
-  return line;
+  char ch;
+  size_t cursor_pos{0};
+
+  while (true) {
+    ch = get_char();
+
+    // newline
+    if (ch == '\n') {
+      std::cout << std::endl;
+      if (!line.empty()) {
+        history.push_back(line);
+        history_idx = history.size();
+      }
+      return line;
+    }
+    // backspace
+    else if (ch == 127) {
+      if (cursor_pos > 0) {
+        line.erase(line.begin() + cursor_pos - 1);
+        cursor_pos--;
+
+        std::cout << "\r> ";
+        std::cout << line << " ";
+        std::cout << "\r> ";
+
+        // move the cursor too at the right position
+        for (size_t i = 0; i < cursor_pos; ++i) {
+          std::cout << line[i];
+        }
+      }
+    }
+    // escape sequence (arrow keys)
+    else if (ch == 27) {
+      if (get_char() == '[') {
+        char arrow = get_char();
+        // we will handle up, down, right and left arrow now
+        if (arrow == 'A' && history_idx > 0) {
+          history_idx--;
+          line = history[history_idx];
+          cursor_pos = line.size();
+          std::cout << "\r\033[K> " << line;
+        } else if (arrow == 'B' && history_idx < history.size() - 1) {
+          history_idx++;
+          line = history_idx < history.size() ? history[history_idx] : "";
+          cursor_pos = line.size();
+          std::cout << "\r\033[K> " << line;
+        } else if (arrow == 'C' && cursor_pos < line.size()) {
+          std::cout << "\033[C";
+          cursor_pos++;
+        } else if (arrow == 'D' && cursor_pos > 0) {
+          std::cout << "\033[D";
+          cursor_pos--;
+        }
+      }
+    } else { // normal chars -> nothing special
+      line.insert(line.begin() + cursor_pos, ch);
+      std::cout << "\r> ";
+      std::cout << line;
+
+      cursor_pos++;
+      std::cout << "\r> ";
+
+        // move the cursor too at the right position
+      for (size_t i = 0; i < cursor_pos; ++i) {
+        std::cout << line[i];
+      }
+    }
+  }
 }
 
 /* split line function
@@ -86,5 +164,6 @@ public function to make signal handling available
 this function is to handle ctrl+c (SIGNINT) and ctrl+z (SIGTSTP) */
 void Ash::setup_signal_handling() {
   signal(SIGINT, handle_sigint);
-  signal(SIGTSTP, SIG_IGN); // for now we just ignore ctrl+z so it does not suspend shell
+  signal(SIGTSTP,
+         SIG_IGN); // for now we just ignore ctrl+z so it does not suspend shell
 }
--- ash.h
+++ ash.h
@@ -1,6 +1,8 @@
 #ifndef ASH
 
 #include <iostream>
+#include <termios.h>
+#include "ash_execvp.h"
 #include <string>
 #include <vector>
 
@@ -11,11 +13,15 @@ public:
   void setup_signal_handling();
 
 private:
+  std::vector<std::string> history;
+  size_t history_idx;
+
   std::string read_line();
   std::vector<std::string> split_line(std::string line);
   int ash_exit();
   int ash_execute(std::vector<std::string> args);
   static void handle_sigint(int sig);
+  char get_char();
 };
 
 #endif // !ASH
